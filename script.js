589b4bd33ee4c532a8f1c0fa0aae7ea61f204aa6
Isaiah Prince <prince1819386@lcsd2kids.org>
2021-04-28 10:55:08 -0600

    Made loop

---
+++ script.js
@@ -0,0 +1,19 @@
+let shoppingList = ["Eggs","Pickles","Ham"];
+console.log(shoppingList[2])
+
+
+for (let i = 0; 1<10 ; i++) {
+    console.log("hi")
+}
+
+
+
+
+let lunchbox = {
+ drink: "Gatorade",
+ sandwich: "Reuben",
+ desert:"Oreos",
+ veggies: "Brocolli",
+ chips: "Sun Chips",
+};
+
